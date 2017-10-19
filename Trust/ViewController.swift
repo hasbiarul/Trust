2632fd9ed0cd05a63c044482d1afa2b28a08a183
arulhasbi <hasbi.fahrul@gmail.com>
2017-10-19 21:49:24 +0700

    Add viewwill dissapear

--- Trust/ViewController.swift
+++ Trust/ViewController.swift
@@ -23,7 +23,10 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         // 
     }
-
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        //
+    }
+    
 }
 
