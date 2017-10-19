d1a74c3636a8abd26c12d0599fec4cf39f83c29b
arulhasbi <hasbi.fahrul@gmail.com>
2017-10-19 21:00:34 +0700

    Initial Commit

---
+++ Trust/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trust
+//
+//  Created by Arul on 10/19/17.
+//  Copyright © 2017 69 Rising. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
